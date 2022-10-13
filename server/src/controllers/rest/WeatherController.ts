6487c3a2e37f37d4deff04180395da3243a36f6b
Ali <raza8r@gmail.com>
2022-10-13 13:24:50 +0500

    created weather forecast endpoints

--- server/src/controllers/rest/WeatherController.ts
+++ server/src/controllers/rest/WeatherController.ts
@@ -1,6 +1,25 @@
 import { Controller, Inject } from "@tsed/di";
-import { Get } from "@tsed/schema";
+import { BodyParams, PathParams } from "@tsed/platform-params";
+import {
+  Delete,
+  Get,
+  Post,
+  Property,
+  Put,
+  Required,
+  Returns,
+} from "@tsed/schema";
+import { WeatherModel } from "src/models/WeatherModel";
 import { WeatherService } from "../../services/WeatherService";
+
+class WeatherParams {
+  @Required() public readonly id: string;
+}
+
+class UpdateWeatherBody {
+  @Property() public readonly id: string;
+  @Property() public readonly data: WeatherModel;
+}
 
 @Controller("/weather")
 export class WeatherController {
@@ -8,7 +27,36 @@ export class WeatherController {
   private weatherService: WeatherService;
 
   @Get("/")
-  get() {
-    return this.weatherService.getWeatherList();
+  @Returns(200, Array).Of(WeatherModel)
+  public async getWeatherList() {
+    return await this.weatherService.getWeatherList();
+  }
+
+  @Get("/:id")
+  @Returns(200, WeatherModel).Of(WeatherModel)
+  public async getWeatherById(@PathParams() params: WeatherParams) {
+    const { id } = params;
+    return await this.weatherService.getWeatherById(id);
+  }
+
+  @Post("/")
+  @Returns(200, WeatherModel).Of(WeatherModel)
+  public async postWeather(@BodyParams() body: WeatherModel) {
+    return await this.weatherService.createWeather(body);
+  }
+
+  @Put("/")
+  @Returns(200, WeatherModel).Of(WeatherModel)
+  public async updateWeather(@BodyParams() body: UpdateWeatherBody) {
+    const { id, data } = body;
+    return await this.weatherService.updateWeather(id, data);
+  }
+
+  @Delete("/:id")
+  @Returns(200, String)
+  public async deleteWeather(@PathParams() params: WeatherParams) {
+    const { id } = params;
+    await this.weatherService.deleteWeather(id);
+    return "Delete record successfully";
   }
 }
